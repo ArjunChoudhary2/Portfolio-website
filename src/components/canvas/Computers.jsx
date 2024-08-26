28092c1d206a8118b207abd7f673520d4be37cbe
ArjunChoudhary2 <105961121+ArjunChoudhary2@users.noreply.github.com>
2024-08-26 11:45:26 +0530

    Landing page and component files created, Hero section Desktop model implemented.

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,25 +3,40 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({isMobile}) => {
   const computer = useGLTF("/desktop_pc/scene.gltf");
-  console.log(computer);
   return (
     <mesh>
       <hemisphereLight intensity={3} groundColor="black" />
       <pointLight intensity={4} />
-      <spotLight  angle={0.12} castShadow penumbra={1} intensity={1} />
+      <spotLight angle={0.12} castShadow penumbra={1} intensity={1} />
       <primitive
         object={computer.scene}
-        scale={.55}
-        position={[0, -3.25, -1.5]}
-        rotation={[-0.01,-0.2,-0.1]}
+        scale={isMobile ? 0.35 : 0.55}
+        position={isMobile ? [0, -1.55, -0.5] :[0, -3.05, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
 };
 
 export const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width : 500px)");
+    
+    setIsMobile(mediaQuery.matches);
+    console.log(isMobile)
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
   return (
     <Canvas
       frameloop="demand"
@@ -35,7 +50,7 @@ export const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile}/>
         <Preload all />
       </Suspense>
     </Canvas>
